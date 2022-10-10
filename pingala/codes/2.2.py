6ac19a858f3046f9512bcbec562bec4543208ed2
AkshithaKola <102524785+AkshithaKola@users.noreply.github.com>
2022-10-10 08:36:42 +0530

    Add files via upload

---
+++ pingala/codes/2.2.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+
+def x(n):
+    if n < 0:
+        return 0
+    elif n <= 1:
+        return 1
+    else:
+        return x(n-1) + x(n-2)
+
+N = 20
+n_arr = np.arange(N)
+x_vec = sp.vectorize(x)
+
+plt.stem(n_arr, x_vec(n_arr))
+plt.grid()
+plt.xlabel('$n$')
+plt.ylabel('$x(n)$')
+plt.savefig('2.2.png')
+plt.show()
---
+++ pingala/codes/2.5.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+
+def x(n):
+    if n < 0:
+        return 0
+    elif n <= 1:
+        return 1
+    else:
+        return x(n-1) + x(n-2)
+
+def y(n):
+    if n < 0:
+        return 0
+    else:
+        return x(n-1) + x(n+1)
+
+N = 20
+n_arr = np.arange(N)
+y_vec = sp.vectorize(y)
+
+plt.stem(n_arr, y_vec(n_arr))
+plt.grid()
+plt.xlabel('$n$')
+plt.ylabel('$y(n)$')
+plt.savefig('2.5.png')
+plt.show()
